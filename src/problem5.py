02bce273a57812cdc21eb51e29b0d31cd0433aab
giambajt <giambajt@rose-hulman.edu>
2019-02-01 11:01:12 -0500

    Done

--- src/problem5.py
+++ src/problem5.py
@@ -3,9 +3,9 @@ Exam 3, problem 5.
 
 Authors: Vibha Alangar, Aaron Wilkin, David Mutchler, Dave Fisher, 
          Matt Boutell, Amanda Stouder, their colleagues and 
-         PUT_YOUR_NAME_HERE.  January 2019.
+         Joshua Giambattista.  January 2019.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -17,7 +17,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.  READ the doc-string for the   is_prime   function defined below.
+# Done: 2.  READ the doc-string for the   is_prime   function defined below.
 # It is the same as you have seen before.
 # After you UNDERSTAND the doc-string (JUST the doc-string, NOT the code),
 # ASKING QUESTIONS AS NEEDED, change the above _TODO_ to DONE.
@@ -148,6 +148,16 @@ def problem5(seq_of_seq):
     # TODO: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    a = []
+    if len(seq_of_seq) > 0:
+        for k in range(len(seq_of_seq)):
+            if len(seq_of_seq[k]) > 0:
+                max_num = seq_of_seq[k][0]
+                for j in range(1,len(seq_of_seq[k])):
+                    if seq_of_seq[k][j] > seq_of_seq[k][j-1]:
+                        max_num = seq_of_seq[k][j]
+                a = a + [max_num]
+    return a
 
 
 ###############################################################################
