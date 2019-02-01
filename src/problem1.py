9f09654693d4c3d92648ab17e6e6375cf1593c73
giambajt <giambajt@rose-hulman.edu>
2019-02-01 11:00:25 -0500

    Done

--- src/problem1.py
+++ src/problem1.py
@@ -3,9 +3,9 @@ Exam 3, problem 1.
 
 Authors: Vibha Alangar, Aaron Wilkin, David Mutchler, Dave Fisher, 
          Matt Boutell, Amanda Stouder, their colleagues and 
-         PUT_YOUR_NAME_HERE.  January 2019.
+         Joshua Giambattista.  January 2019.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -126,7 +126,7 @@ def problem1(n):
       :rtype: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # Done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # **** IMPORTANT ****:
     # We supplied a   fibonacci   function above.
@@ -139,6 +139,19 @@ def problem1(n):
     # and THEN try the full-credit version.
     # As always, CONTINUE to the next problem if you are STUCK on this one.
     # -------------------------------------------------------------------------
+    first = 0
+    second  = 1
+    while True:
+        if first + second > n:
+            if second < n:
+                return second
+            else:
+                return first
+        else:
+            b = first
+            m = second
+            first = m
+            second = b+m
 
 
 ###############################################################################
